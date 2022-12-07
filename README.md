1b2a89530b784ac7dd4a56b5c38fa34dd9e7500a
윤인희(대학원생-디지털마케팅전공) <113741161+Innieee@users.noreply.github.com>
2022-12-07 13:42:50 +0000

    상세페이지 프로젝트 완료

--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 
 22년 디지털 마케팅 프로그래밍 학기 과제입니다.
 
-일반 문장('<p></p>')
+록시땅 라벤더 핸드크림 상세 페이지를 만들었습니다.
+
+- 국민대학교 경영대학원 디지털 마케팅 전공 U2022064 윤인희.
+
+
+
+
 
 ## Reference
 
-[Google](https://www.google.com)
+[Loccitane](https://kr.loccitane.com/%EB%9D%BC%EB%B2%A4%EB%8D%94-%ED%95%B8%EB%93%9C-%ED%81%AC%EB%A6%BC-75ml-15MA075L20.html)
---
+++ script.js
@@ -0,0 +1,22 @@
+function login(){document.write("Login Page");
+}
+
+function mypage() {
+  document.write("My Page");
+}
+
+function order() {
+  document.write("Order Confirm");
+}
+
+function cart() {
+  document.write("Your Cart is Empty");
+}
+
+function buy() {
+  document.write("Wish List");
+}
+
+function keyimg() {
+  document.write("Sorry... Image is now on preparation...");
+}
